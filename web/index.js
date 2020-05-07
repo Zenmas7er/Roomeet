7ea55c6ad5b33cc0b34860b64c02681fb572508d
Zenmas7er <nholung96@gmail.com>
2020-05-07 16:26:01 -0400

    Started Rectangle Tool
    It prints out rectangles as a brush tip, so it needs work. The goal is to implement an MSPaint-styled rectangle tool.

--- web/index.js
+++ web/index.js
@@ -17,8 +17,8 @@ const ctx = canvas.getContext('2d');
 	
 // Resizes the canvas to the available size of the window. 
 function resize(){ 
-ctx.canvas.width = window.innerWidth; 
-ctx.canvas.height = window.innerHeight; 
+    ctx.canvas.width = window.innerWidth; 
+    ctx.canvas.height = window.innerHeight; 
 } 
 	
 // Stores the initial position of the cursor 
@@ -32,19 +32,23 @@ let paint = false;
 // an event e is triggered to the coordinates where 
 // the said event is triggered. 
 function getPosition(event){ 
-coord.x = event.clientX - canvas.offsetLeft; 
-coord.y = event.clientY - canvas.offsetTop; 
+    coord.x = event.clientX - canvas.offsetLeft; 
+    coord.y = event.clientY - canvas.offsetTop; 
 } 
 
 // The following functions toggle the flag to start 
 // and stop drawing 
 function startPainting(event){ 
-paint = true; 
-getPosition(event); 
+    paint = true; 
+    getPosition(event); 
 } 
 function stopPainting(){ 
-paint = false; 
+    paint = false; 
 }
+
+// Records starting points of the cursor for the Rectangle tool
+// var StartX = coord.x;
+// var StartY = coord.y;
 
 // function componentToHex(c) {
 //     var hex = c.toString(16);
@@ -56,47 +60,60 @@ paint = false;
 // }
 
 // Alert test to see what hexadecimal color is coming out
-alert( tx.strokeStyle = getRGB(document.getElementById('red').value, document.getElementById('green').value, document.getElementById('blue')));
+// alert( tx.strokeStyle = getRGB(document.getElementById('red').value, document.getElementById('green').value, document.getElementById('blue')));
 	
 function sketch(event){ 
-if (!paint) return; 
-ctx.beginPath(); 
+    if (!paint) 
+        return; 
+    ctx.beginPath(); 
 
-// Sets the end of the lines drawn 
-// to a round shape. 
-if (document.getElementById('radio1').checked) {
-    rate_value = document.getElementById('radio1').value;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = 'rgb(' + document.getElementById('red').value + ',' + document.getElementById('green').value + ',' + document.getElementById('blue').value +')';
-    ctx.lineWidth = document.getElementById('tipsize').value;   
-}
+    // Sets the end of the lines drawn 
+    // to a round shape. 
+    if (document.getElementById('radio1').checked) {
+        rate_value = document.getElementById('radio1').value;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'rgb(' + document.getElementById('red').value + ',' + document.getElementById('green').value + ',' + document.getElementById('blue').value +')';
+        ctx.lineWidth = document.getElementById('tipsize').value;   
+    }
 
-if (document.getElementById('radio2').checked) {
-    rate_value = document.getElementById('radio2').value;
-    ctx.lineCap = 'square';
-    ctx.strokeStyle = 'rgb(' + document.getElementById('red').value + ',' + document.getElementById('green').value + ',' + document.getElementById('blue').value +')';
-    ctx.lineWidth = document.getElementById('tipsize').value;   
-}
+    if (document.getElementById('radio2').checked) {
+        rate_value = document.getElementById('radio2').value;
+        ctx.lineCap = 'square';
+        ctx.strokeStyle = 'rgb(' + document.getElementById('red').value + ',' + document.getElementById('green').value + ',' + document.getElementById('blue').value +')';
+        ctx.lineWidth = document.getElementById('tipsize').value;   
+    }
 
-if (document.getElementById('radio3').checked) {
-    rate_value = document.getElementById('radio3').value;
-    ctx.strokeStyle = 'White';
-    ctx.lineWidth = 2 * document.getElementById('tipsize').value;   
-}
-	
-// The cursor to start drawing 
-// moves to this coordinate 
-ctx.moveTo(coord.x, coord.y); 
+    if (document.getElementById('radio3').checked) {
+        rate_value = document.getElementById('radio3').value;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'White';
+        ctx.lineWidth = 2 * document.getElementById('tipsize').value;   
+    }
 
-// The position of the cursor 
-// gets updated as we move the 
-// mouse around. 
-getPosition(event); 
+    if (document.getElementById('radio4').checked) {
+        rate_value = document.getElementById('radio4').value;
+        ctx.strokeStyle = 'rgb(' + document.getElementById('red').value + ',' + document.getElementById('green').value + ',' + document.getElementById('blue').value +')';
+        ctx.lineWidth = document.getElementById('tipsize').value;
+        
+        // var width = coord.x - StartX;
+        // var height = coord.y - StartY;
 
-// A line is traced from start 
-// coordinate to this coordinate 
-ctx.lineTo(coord.x , coord.y); 
-	
-// Draws the line. 
-ctx.stroke(); 
+        // ctx.strokeRect(coord.x, coord.y, width, height);
+    }
+        
+    // The cursor to start drawing 
+    // moves to this coordinate 
+    ctx.moveTo(coord.x, coord.y); 
+
+    // The position of the cursor 
+    // gets updated as we move the 
+    // mouse around. 
+    getPosition(event); 
+
+    // A line is traced from start 
+    // coordinate to this coordinate 
+    ctx.lineTo(coord.x , coord.y); 
+        
+    // Draws the line. 
+    ctx.stroke(); 
 } 
